be3e5c9e160794298c6d8264aa116eedbd62a2a8
Mohammad Soqar <mnsoqar1@gmail.com>
2023-06-06 12:59:39 +0300

    Migrations

---
+++ CodyleOffical.DataAccess/Migrations/20230605203339_addOnlineOptionToDb.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodyleOffical.Migrations
+{
+    /// <inheritdoc />
+    public partial class addOnlineOptionToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EventLiveLink",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OnlineEvent",
+                table: "Events",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Sponsors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sponsors", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sponsors");
+
+            migrationBuilder.DropColumn(
+                name: "EventLiveLink",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "OnlineEvent",
+                table: "Events");
+        }
+    }
+}
